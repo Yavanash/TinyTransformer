Fixed Normalization

model/inference.py
@@ -118,6 +118,7 @@ MAX_SEQ_LEN = 54
 MAX_LEN = 25
 
 tgt_vocab = testdata.eng_vocab
+src_vocab = testdata.fra_vocab
 preprocess = testdata.preprocess
 
 model = InferenceTransformer(D_MODEL, NUM_HEADS, NUM_LAYERS, D_FF, MAX_SEQ_LEN, SRC_VOCAB_SIZE, TGT_VOCAB_SIZE)
@@ -146,10 +147,11 @@ def validate(model, criterion, test_loader):
     
     return test_losses
 
-def translate(model, src, tgt_vocab = tgt_vocab, preprocess = preprocess):
+def translate(model, src, src_vocab = src_vocab, tgt_vocab = tgt_vocab, preprocess = preprocess):
+    model.eval()
     pre = preprocess(src)
-    tokens = tgt_vocab.encode(pre)
-    tensor = torch.tensor(tokens, dtype=torch.long)
+    tokens = src_vocab.encode(pre)
+    tensor = torch.tensor(tokens, dtype=torch.long, device=device)
 
     beams = model(tensor.unsqueeze(0))
 
@@ -161,7 +163,8 @@ def translate(model, src, tgt_vocab = tgt_vocab, preprocess = preprocess):
 
 def main():
     src = input("Enter sentence to translate: ")
-    outputs = translate(model, src)
+    with torch.no_grad():
+        outputs = translate(model, src)
     print(outputs)
 
 if __name__ == "__main__":

model/main.py
@@ -174,12 +174,12 @@ class EncoderLayer(nn.Module):
 
     def forward(self, x, mask=None):
         # input x = embed + positional encoding = [bs, sl, d]
-        x = self.norm1(x)
-        z = self.mha(x, x, x, mask)
+        x1 = self.norm1(x)
+        z = self.mha(x1, x1, x1, mask)
         z_norm1 = self.dropout(z) + x # residual connections
 
-        z_norm1 = self.norm2(z_norm1)
-        z = self.ffn(z_norm1)
+        z_norm1_1 = self.norm2(z_norm1)
+        z = self.ffn(z_norm1_1)
         z_norm2 = self.dropout(z) + z_norm1
 
         return z_norm2
@@ -202,16 +202,16 @@ class DecoderLayer(nn.Module):
         # tgt_mask = eng mask = causal + padding mask
         # non auto regressive in training
 
-        z = self.norm1(x)
-        z = self.masked_mha(x, x, x, tgt_mask)
+        z0 = self.norm1(x)
+        z = self.masked_mha(z0, z0, z0, tgt_mask)
         z1 = self.dropout(z) + x
         
-        z1 = self.norm2(z1)
-        z2 = self.cross_mha(z1, enc_output, enc_output, src_mask)
+        z1_1 = self.norm2(z1)
+        z2 = self.cross_mha(z1_1, enc_output, enc_output, src_mask)
         z3 = self.dropout(z2) + z1
 
-        z3 = self.norm3(z3)
-        y = self.ffn(z3)
+        z3_1 = self.norm3(z3)
+        y = self.ffn(z3_1)
         out = self.dropout(y) + z3
 
         return out